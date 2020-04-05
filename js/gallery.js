534ee78e9c47373f220de27d05f158ce40ec2844
Zhiyu Michael Wang <53185549+Imnphliogablet@users.noreply.github.com>
2020-04-05 08:11:49 +0800

    Add files via upload

---
+++ js/gallery.js
@@ -0,0 +1,439 @@
+var galleryItems = [
+    {
+        id: 'w01',
+        type: 'inner-grid-wide',
+        items: [
+            {
+                id: 'w01-w',
+                type: 'grid-item',
+                subtype: 'grid-item-wide',
+                img: {
+                    xs: 'https://i.picsum.photos/id/1000/420/200.jpg',
+                    s: 'https://i.picsum.photos/id/1000/920/410.jpg',
+                    m: 'https://i.picsum.photos/id/1000/1200/500.jpg',
+                    lg: 'https://i.picsum.photos/id/1000/1920/600.jpg'
+                },
+                content: {
+                    img: 'https://i.picsum.photos/id/1000/1920/600.jpg',
+                    title: 'popbox-w01-w',
+                    text: 'thsi afa f the e dis qcuopqer aiao  of thae pic tiaera',
+                    link: 'article.html'
+                }
+            }
+        ]
+    },{
+        id: '6401',
+        type: 'inner-grid-6-4',
+        items: [
+            {
+                id: '6401-6',
+                type: 'grid-item',
+                subtype: 'grid-item-6',
+                img: {
+                    xs: 'https://i.picsum.photos/id/1000/250/200.jpg',
+                    s: 'https://i.picsum.photos/id/1000/548/410.jpg',
+                    m: 'https://i.picsum.photos/id/1000/714/500.jpg',
+                    lg: 'https://i.picsum.photos/id/1000/1143/600.jpg'
+                },
+                content: {
+                    img: 'https://i.picsum.photos/id/1000/1143/600.jpg',
+                    title: 'popBox-6401-6',
+                    text: 'thsfhaufoa fiso sioo 0aa0i ss aiao  of thae pic tiaera',
+                    link: 'article.html'               
+                },
+            },{
+                id: '6401-4',
+                type: 'grid-item',
+                subtype: 'grid-item-4',
+                img: {
+                    xs:'https://i.picsum.photos/id/1000/166/200.jpg',
+                    s: 'https://i.picsum.photos/id/1000/364/410.jpg',
+                    m: 'https://i.picsum.photos/id/1000/474/500.jpg',
+                    lg: 'https://i.picsum.photos/id/1000/758/600.jpg'
+                },
+                content: {
+                    img: 'https://i.picsum.photos/id/1000/758/600.jpg',
+                    title: 'popBox-6401-4',
+                    text: 'thsfhaufoa fiso sioo 0aa0i ss aiao  of thae pic tiaera',
+                    link: 'article.html'
+                }
+            }        
+        ]
+    },{
+        id: '22801',
+        type: 'inner-grid-2-2-8',
+        items: [
+            {
+                id: '22801-2a',
+                type: 'grid-item-s',
+                subtype: 'grid-item-2',
+                img: {
+                    xs: 'https://i.picsum.photos/id/1000/145/100.jpg',
+                    s: 'https://i.picsum.photos/id/1000/318/205.jpg',
+                    m: 'https://i.picsum.photos/id/1000/414/250.jpg',
+                    lg: 'https://i.picsum.photos/id/1000/663/295.jpg'
+                },
+                content: {
+                    img: 'https://i.picsum.photos/id/1000/1143/600.jpg',
+                    title: 'popBox-22801-2a',
+                    text: 'thsfhaufoa fiso sioo 0aa0i ss aiao  of thae pic tiaera',
+                    link: 'article.html'               
+                },
+            },{
+                id: '22801-2b',
+                type: 'grid-item-s',
+                subtype: 'grid-item-2',
+                img: {
+                    xs: 'https://i.picsum.photos/id/1000/145/100.jpg',
+                    s: 'https://i.picsum.photos/id/1000/318/205.jpg',
+                    m: 'https://i.picsum.photos/id/1000/414/250.jpg',
+                    lg: 'https://i.picsum.photos/id/1000/663/295.jpg'
+                },
+                content: {
+                    img: 'https://i.picsum.photos/id/1000/1143/600.jpg',
+                    title: 'popBox-22801-2b',
+                    text: 'thsfhaufoa fiso sioo 0aa0i ss aiao  of thae pic tiaera',
+                    link: 'article.html'               
+                },
+            },{
+                id: '22801-8',
+                type: 'grid-item-lg',
+                subtype: 'grid-item-8',
+                img: {
+                    xs: 'https://i.picsum.photos/id/1000/271/200.jpg',
+                    s: 'https://i.picsum.photos/id/1000/594/410.jpg',
+                    m: 'https://i.picsum.photos/id/1000/774/500.jpg',
+                    lg: 'https://i.picsum.photos/id/1000/1239/600.jpg'
+                },
+                content: {
+                    img: 'https://i.picsum.photos/id/1000/1239/600.jpg',
+                    title: 'popBox-22801-8',
+                    text: 'thsfhaufoa fiso sioo 0aa0i ss aiao  of thae pic tiaera',
+                    link: 'article.html'               
+                },
+            }        
+        ]
+    },{
+        id: '6402',
+        type: 'inner-grid-6-4',
+        items: [
+            {
+                id: '6402-6',
+                type: 'grid-item',
+                subtype: 'grid-item-6',
+                img: {
+                    xs: 'https://i.picsum.photos/id/1000/250/200.jpg',
+                    s: 'https://i.picsum.photos/id/1000/548/410.jpg',
+                    m: 'https://i.picsum.photos/id/1000/714/500.jpg',
+                    lg: 'https://i.picsum.photos/id/1000/1143/600.jpg'
+                },
+                content: {
+                    img: 'https://i.picsum.photos/id/1000/1143/600.jpg',
+                    title: 'popBox-6402-6',
+                    text: 'thsfhaufoa fiso sioo 0aa0i ss aiao  of thae pic tiaera',
+                    link: 'article.html'               
+                },
+            },{
+                id: '6402-4',
+                type: 'grid-item',
+                subtype: 'grid-item-4',
+                img: {
+                    xs:'https://i.picsum.photos/id/1000/166/200.jpg',
+                    s: 'https://i.picsum.photos/id/1000/364/410.jpg',
+                    m: 'https://i.picsum.photos/id/1000/474/500.jpg',
+                    lg: 'https://i.picsum.photos/id/1000/758/600.jpg'
+                },
+                content: {
+                    img: 'https://i.picsum.photos/id/1000/758/600.jpg',
+                    title: 'popBox-6402-4',
+                    text: 'thsfhaufoa fiso sioo 0aa0i ss aiao  of thae pic tiaera',
+                    link: 'article.html'
+                }
+            }        
+        ]
+    },{
+        id: '5501',
+        type: 'inner-grid-5-5',
+        items: [
+            {
+                id: '5501-5a',
+                type: 'grid-item',
+                subtype: 'grid-item-5',
+                img: {
+                    xs: 'https://i.picsum.photos/id/1000/208/200.jpg',
+                    s: 'https://i.picsum.photos/id/1000/456/410.jpg',
+                    m: 'https://i.picsum.photos/id/1000/594/500.jpg',
+                    lg: 'https://i.picsum.photos/id/1000/951/600.jpg'
+                },
+                content: {
+                    img: 'https://i.picsum.photos/id/1000/951/600.jpg',
+                    title: 'popBox-5501-5a',
+                    text: 'thsfhaufoa fiso sioo 0aa0i ss aiao  of thae pic tiaera',
+                    link: 'article.html'               
+                },
+            },{
+                id: '5501-5b',
+                type: 'grid-item',
+                subtype: 'grid-item-5',
+                img: {
+                    xs: 'https://i.picsum.photos/id/1000/208/200.jpg',
+                    s: 'https://i.picsum.photos/id/1000/456/410.jpg',
+                    m: 'https://i.picsum.photos/id/1000/594/500.jpg',
+                    lg: 'https://i.picsum.photos/id/1000/951/600.jpg'
+                },
+                content: {
+                    img: 'https://i.picsum.photos/id/1000/951/600.jpg',
+                    title: 'popBox-5501-5b',
+                    text: 'thsfhaufoa fiso sioo 0aa0i ss aiao  of thae pic tiaera',
+                    link: 'article.html'
+                }
+            }        
+        ]
+    },
+]
+
+
+
+
+var gridItemTarget = $('#grid-section')
+
+for (ii = 0; ii < galleryItems.length; ii++) {
+    var it = galleryItems[ii]
+    
+    var grid_id = it.id
+    var grid_type = it.type
+    var grid_items = it.items
+
+    var renderGrid = '<div class="' + grid_type + '">'
+    for (jj = 0; jj < grid_items.length; jj++) {
+        var item = grid_items[jj]
+        var item_id = item.id
+        var item_type = item.type
+        var item_subtype = item.subtype
+        var item_img = item.img
+        var item_content = item.content
+
+        var renderPicture = 
+        '<picture onclick="popBox(\'' + item_id + '\')">' + 
+            '<source srcset="' + item_img.xs + '"media="(max-width: 420px)">' +
+            '<source srcset="' + item_img.s + '"media="(max-width: 920px)">' +
+            '<source srcset="' + item_img.m + '"media="(max-width: 1200px)">' +
+            '<img srcset="' + item_img.lg + '"class="' + item_subtype + '">' +
+        '</picture>'
+
+        var renderPopbox = 
+        '<div id="popBox-' + item_id + '"class="popBox" onclick="closeBox(\'' + item_id + '\')">' +                            
+            '<div class="popBox-content">' +
+                '<img src="' + item_content.img + '">' +
+                '<h4> ' + item_content.title + '</h4>' +
+                '<p>' + 
+                    item_content.text + 
+                    '<a href="' + item_content.link + '" class="popBox-extend-content">-learn more-</a>' +
+                '</p>' +                    
+            '</div>' +                
+        '</div>'
+
+        var renderItem = 
+        '<div class="' + item_type + '">' + 
+            renderPicture +
+            renderPopbox +
+        '</div>'
+
+        renderGrid += renderItem
+    }
+    renderGrid += '</div>'
+    
+    gridItemTarget.append(renderGrid)
+   }
+
+/** sample of inner grid wide
+ * <div class="inner-grid-wide">
+        <div class="grid-item ">
+            
+            <picture onclick="popBox('w-0001')">
+                <source srcset="https://i.picsum.photos/id/1000/420/200.jpg" media="(max-width: 420px)">
+                <source srcset="https://i.picsum.photos/id/1000/920/410.jpg" media="(max-width: 920px)">
+                <source srcset="https://i.picsum.photos/id/1000/1200/500.jpg" media="(max-width: 1200px)">
+                <img srcset="https://i.picsum.photos/id/1000/1920/600.jpg" class="grid-item-wide">
+            </picture>
+            
+            <div id="popBox-w-0001" class="popBox" onclick="closeBox('w-0001')">                            
+                <div class="popBox-content">
+                    <img src="https://i.picsum.photos/id/1000/1920/600.jpg">
+                    <h4>popBox-w-0001</h4>
+                    <p>
+                        thsi afa f the e dis qcuopqer aiao  of thae pic tiaera 
+                        <a href="article.html" class="popBox-extend-content">-learn more-</a>
+                    </p>
+                    
+                </div>
+            </div>                        
+        </div>
+    </div>
+ */
+
+/**sample of inner grid 64
+ * <div class="inner-grid-6-4">
+        <div class="grid-item">
+            
+                <picture onclick="popBox('6401-6')">
+                    <source srcset="https://i.picsum.photos/id/1000/250/200.jpg" media="(max-width: 420px)">
+                    <source srcset="https://i.picsum.photos/id/1000/548/410.jpg" media="(max-width: 920px)">
+                    <source srcset="https://i.picsum.photos/id/1000/714/500.jpg" media="(max-width: 1200px)">
+                    <img srcset="https://i.picsum.photos/id/1000/1143/600.jpg" class="grid-item-6">
+                </picture>
+
+                <div id="popBox-6401-6" class="popBox" onclick="closeBox('6401-6')">                            
+                    <div class="popBox-content">
+                        <img src="https://i.picsum.photos/id/1000/1143/600.jpg">
+                        <h4>popBox-6401-6</h4>
+                        <p>
+                            thsfhaufoa fiso sioo 0aa0i ss aiao  of thae pic tiaera 
+                            <a href="article.html" class="popBox-extend-content">-learn more-</a>
+                        </p>                        
+                    </div>
+                </div>       
+
+        </div>
+
+        <div class="grid-item">
+            
+            <picture onclick="popBox('6401-4')">
+                <source srcset="https://i.picsum.photos/id/1000/166/200.jpg" media="(max-width: 420px)">
+                <source srcset="https://i.picsum.photos/id/1000/364/410.jpg" media="(max-width: 920px)">
+                <source srcset="https://i.picsum.photos/id/1000/474/500.jpg" media="(max-width: 1200px)">
+                <img srcset=" https://i.picsum.photos/id/1000/758/600.jpg" class="grid-item-4">
+            </picture>
+
+            <div id="popBox-6401-4" class="popBox" onclick="closeBox('6401-4')">                            
+                <div class="popBox-content">
+                    <img src="https://i.picsum.photos/id/1000/758/600.jpg">
+                    <h4> popBox-6401-4</h4>
+                    <p>
+                        thsfhaufoa fiso sioo 0aa0i ss aiao  of thae pic tiaera 
+                        <a href="article.html" class="popBox-extend-content">-learn more-</a>
+                    </p>                    
+                </div>                
+            </div> 
+            
+        </div>
+
+    </div>
+ */
+
+/** sample of inner grid 228
+  * <div class=" inner-grid-2-2-8">
+        <div class="grid-item-s">
+            
+            <picture onclick="popBox('22801-2a')">
+                <source srcset="https://i.picsum.photos/id/1000/145/100.jpg" media="(max-width: 420px)">
+                <source srcset="https://i.picsum.photos/id/1000/318/205.jpg" media="(max-width: 920px)">
+                <source srcset="https://i.picsum.photos/id/1000/414/250.jpg" media="(max-width: 1200px)">
+                <img srcset="https://i.picsum.photos/id/1000/663/295.jpg" class="grid-item-2">
+            </picture>
+            <div id="popBox-22801-2a" class="popBox" onclick="closeBox('22801-2a')">                            
+                <div class="popBox-content">
+                    <img src="https://i.picsum.photos/id/1000/1143/600.jpg">
+                    <h4> popBox-22801-2a</h4>
+                    <p>
+                        thsfhaufoa fiso sioo 0aa0i ss aiao  of thae pic tiaera 
+                        <a href="article.html" class="popBox-extend-content">-learn more-</a>
+                    </p>                                
+                </div>
+            </div>   
+
+        </div>
+
+        <div class="grid-item-s">
+            
+            <picture onclick="popBox('228-2002')">
+                <source srcset="https://i.picsum.photos/id/1000/145/100.jpg" media="(max-width: 420px)">
+                <source srcset="https://i.picsum.photos/id/1000/318/205.jpg" media="(max-width: 920px)">
+                <source srcset=" https://i.picsum.photos/id/1000/414/250.jpg" media="(max-width: 1200px)">
+                <img srcset=" https://i.picsum.photos/id/1000/663/295.jpg" class="grid-item-2">
+            </picture>
+
+            <div id="popBox-228-2002" class="popBox" onclick="closeBox('228-2002')">                            
+                <div class="popBox-content">
+                    <img src="https://i.picsum.photos/id/1000/1326/590.jpg">
+                    <h4> popBox-228-2002</h4>
+                    <p>
+                        thsfhaufoa fiso sioo 0aa0i ss aiao  of thae pic tiaera 
+                        <a href="article.html" class="popBox-extend-content">-learn more-</a>
+                    </p>                                
+                </div>
+            </div> 
+        
+        </div>
+
+        <div class="grid-item" style=" grid-row: 1/3; grid-column: 2/3">
+            
+            <picture onclick="popBox('228-8001')">
+                <source srcset="https://i.picsum.photos/id/1000/271/200.jpg" media="(max-width: 420px)">
+                <source srcset="https://i.picsum.photos/id/1000/594/410.jpg" media="(max-width: 920px)">
+                <source srcset="https://i.picsum.photos/id/1000/774/500.jpg" media="(max-width: 1200px)">
+                <img srcset="https://i.picsum.photos/id/1000/1239/600.jpg" class="grid-item-8">
+            </picture>
+            <div id="popBox-228-8001" class="popBox" onclick="closeBox('228-8001')">                            
+                <div class="popBox-content">
+                    <img src="https://i.picsum.photos/id/1000/1239/600.jpg">
+                    <h4> popBox-228-8001</h4>
+                    <p>
+                        thsfhaufoa fiso sioo 0aa0i ss aiao  of thae pic tiaera 
+                        <a href="article.html" class="popBox-extend-content">-learn more-</a>
+                    </p>                                
+                </div>
+            </div> 
+        
+        </div>
+    </div>
+  */
+
+/** sample of inner grid 55
+ * <div class=" inner-grid-5-5">
+        <div class="grid-item">
+            
+            <picture onclick="popBox('55-a001')">
+                <source srcset="https://i.picsum.photos/id/1000/208/200.jpg" media="(max-width: 420px)">
+                <source srcset="https://i.picsum.photos/id/1000/456/410.jpg" media="(max-width: 920px)">
+                <source srcset="https://i.picsum.photos/id/1000/594/500.jpg" media="(max-width: 1200px)">
+                <img srcset="https://i.picsum.photos/id/1000/951/600.jpg" class="grid-item-5">
+            </picture>
+            <div id="popBox-55-a001" class="popBox" onclick="closeBox('55-a001')">                            
+                <div class="popBox-content">
+                    <img src="https://i.picsum.photos/id/1000/758/600.jpg">
+                    <h4> popBox-55-a001</h4>
+                    <p>
+                        thsfhaufoa fiso sioo 0aa0i ss aiao  of thae pic tiaera 
+                        <a href="article.html" class="popBox-extend-content">-learn more-</a>
+                    </p>
+                    
+                </div>
+            </div> 
+        
+
+        </div>
+        <div class="grid-item">
+            
+                <picture onclick="popBox('55-b001')">
+                    <source srcset="https://i.picsum.photos/id/1000/208/200.jpg" media="(max-width: 420px)">
+                    <source srcset="https://i.picsum.photos/id/1000/456/410.jpg" media="(max-width: 920px)">
+                    <source srcset="https://i.picsum.photos/id/1000/594/500.jpg" media="(max-width: 1200px)">
+                    <img srcset="https://i.picsum.photos/id/1000/951/600.jpg" class="grid-item-5">
+                </picture>
+                <div id="popBox-55-b001" class="popBox" onclick="closeBox('55-b001')">                            
+                    <div class="popBox-content">
+                        <img src="https://i.picsum.photos/id/1000/758/600.jpg">
+                        <h4> popBox-55-b001</h4>
+                        <p>
+                            thsfhaufoa fiso sioo 0aa0i ss aiao  of thae pic tiaera 
+                            <a href="article.html" class="popBox-extend-content">-learn more-</a>
+                        </p>
+                        
+                    </div>
+                </div> 
+        
+
+        </div>
+    </div>
+ */
